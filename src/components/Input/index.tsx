8f13ff5d53d375d76c1b05877d25c05819739b25
Luís Gustavo <luis.gustavo_18@outlook.com>
2020-08-29 14:29:16 -0300

    UX do Input e useCallback

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react';
 import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
 
@@ -15,13 +15,24 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     * Permite acessar as propriedades do elemento como na DOM
     * precisa ser atribuída ao input para recuperar esses dados
     */
-    const inputRef = useRef(null);
-    
+   const inputRef = useRef<HTMLInputElement>(null);
+   
+   const [isFocused, setIsFocused] = useState(false);
+   const [isFilled, setIsFilled] = useState(false);
     /*
     * Recebe o nome do campo como argumento e irá retornar alguns dados
     * que serão usados para receber o valor do input e fazer outras tratativas
     */
     const { fieldName, defaultValue, error, registerField } = useField(name);
+
+    const handleInputFocus = useCallback(() => {
+        setIsFocused(true);
+    }, []);
+
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false);
+        setIsFilled(!!inputRef.current?.value);
+    }, []);
 
     //assim que o componente for exibido em tela, ela recebe o registerField
     useEffect(() => {
@@ -43,9 +54,15 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     */
 
     return (
-        <Container>
+        <Container isFilled={isFilled} isFocused={isFocused}>
             { Icon && <Icon size={20} /> }
-            <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+            <input 
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                defaultValue={defaultValue} 
+                ref={inputRef} 
+                {...rest} 
+            />
         </Container>
     );
 };
